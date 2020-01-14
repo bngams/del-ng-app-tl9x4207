4f4fd1a39e308f95d9ffcb2a07dc2d7bf324adab
bngams <n.boris@devidia.net>
2020-01-14 16:28:16 +0100

    template syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-app-sample';
+  btnDisabled = false;
+  today = Date.now();
+  txtDisplayed = false;
+  products = [
+    {
+      name: 'P1',
+      price: 12
+    },
+    {
+      name: 'P2',
+      price: 10
+    },
+    {
+      name: 'P3',
+      price: 15
+    }
+  ];
+
+  actionBtn(event): void {
+    console.log('Btn click', event);
+    // edit btn status
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  displayText(event): void {
+    this.txtDisplayed = true;
+  }
 }
